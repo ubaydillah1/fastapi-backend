c5ac3bbd960168b711a40477882ef1e7f3a944c4
ubaydillah1 <ubaydillah1737@gmail.com>
2026-07-09 20:29:23 +0700

    feat: add users and posts db

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,8 @@
 import uuid
-from sqlalchemy import Integer, String, Uuid
-from sqlalchemy.orm import Mapped, mapped_column
+
+from typing import List
+from sqlalchemy import Integer, String, Uuid, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models import Base
 
@@ -10,3 +12,15 @@ class User(Base):
     id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
     name: Mapped[str] = mapped_column(String(30))
     age: Mapped[int] = mapped_column(Integer)
+    posts: Mapped[List["Post"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String(30))
+    description: Mapped[str] = mapped_column(String(255))
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="posts")
